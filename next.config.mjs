8e1acb588cb94907052d753c75b1e2e2304ddfca
shahkhushi0307 <shahkhushi0307@users.noreply.github.com>
2026-08-08 05:09:31 +0000

    deploy: 2cf03a148685031da65b3ee2f95cd1941d5037cf

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  trailingSlash: true,
-};
-
-export default nextConfig;
